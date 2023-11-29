7295dc4dd3fc81688f5627ad887fcacc3247fe75
Zlatko Naumoski <zn2601@gmail.com>
2023-11-29 12:46:20 +0100

    install geocoder gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :index]
 
   def home
+  end
+
+  def index
+    @posts = Post.all
+
   end
 end
