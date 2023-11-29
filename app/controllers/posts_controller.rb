a794fb031956ca5f58e5174ee1a28d7ac605d986
Anna Bothe <annabothe0@gmail.com>
2023-11-29 16:40:07 +0100

    env ignore

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[post_params])
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :discription, :category)
+  end
 end
