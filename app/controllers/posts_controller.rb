2c9d99a76741e8eb8a2df6131395529020790568
Vincent Bohec <vbohec@yahoo.fr>
2023-11-29 15:34:49 +0100

    updated the post show page until the image attached

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,14 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    if @post.asker?
+      welcome_message = "They would really appreciate some help with ##{@post.category}"
+      button_text = "Help out!"
+    else
+      welcome_message = "They would love to help with ##{@post.category}"
+      button_text = "Reach out!"
+    end
+    render locals: { welcome_message: welcome_message, button_text: button_text }
   end
 
 end
-
-
