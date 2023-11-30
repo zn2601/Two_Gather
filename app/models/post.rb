cde3ebbd6cc0039ad4903dec61d45568ad1352c4
Anna Bothe <annabothe0@gmail.com>
2023-11-30 15:15:35 +0100

    added post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many_attached :photos
+
+  CATEGORIES = ["gardening", "cooking", "DIY", "cleaning"]
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :title, presence: true, length: { minimum: 40, maximum: 70 }
+  validates :description, presence: true, length: { minimum: 150, maximum: 250 }
+  validates :asker, presence: true
 end
