df83c37ec0c1210e1ae56d7195eeb54ce6809e10
Anna Bothe <annabothe0@gmail.com>
2023-12-01 14:51:41 +0100

    updated seeds with new info and model validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,45 +20,108 @@ user3 = User.create(email: "vincent@admin.com", password: "123123", name: "admin
 puts "creating posts"
 post1 =
 Post.create!(
-  title: "Need help with my garden",
-  category: "gardening",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  title: "Need Quick Recipe Ideas",
+  category: "cooking",
+  description: "Running out of dinner inspiration after a long day. Any favorite easy recipes to share? Preferably something quick and delicious. Open to all cuisines!",
   user_id: user1.id,
   solved: false,
   asker: true
 )
 Post.create!(
-  title: "Can someone help me with the dishes?",
-  category: "household",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  title: "Gardening Tips Needed",
+  category: "gardening",
+  description: "I'm a newbie gardener seeking advice on growing tomatoes in a small backyard. Any green thumbs out there willing to share tips on soil, watering, and sunlight?",
   user_id: user2.id,
   solved: false,
   asker: true
 )
 
 Post.create!(
-  title: "Could someone please help me with the grocery shopping?",
-  category: "household",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-  user_id: user2.id,
-  solved: false,
-  asker: true
-)
-
-Post.create!(
-  title: "Can someone help me with the dusting?",
-  category: "household",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  title: "Babysitter Cancellation",
+  category: "parenting",
+  description: "Urgently need a babysitter tonight! Last-minute cancellation, and we have plans. Our kids are well-behaved; just need someone trustworthy for a few hours. Compensation provided!",
   user_id: user3.id,
   solved: false,
   asker: true
 )
 
 Post.create!(
-  title: "Need help with hanging the picture on the wall",
+  title: "Bike Repair Help",
+  category: "DIY",
+  description: "Disaster struck on my commute! Flat tire on my bike, and I'm clueless about repairs. Anyone skilled in bike repairs willing to lend a hand or share tools?",
+  user_id: user4.id,
+  solved: false,
+  asker: true
+)
+
+Post.create!(
+  title: "Computer Glitch",
+  category: "tech",
+  description: "Tech trouble! My computer won't start, and I have work to finish. Any IT-savvy neighbors available for a quick troubleshooting session?",
+  user_id: user5.id,
+  solved: false,
+  asker: true
+)
+
+Post.create!(
+  title: "Artistic Advice Needed",
+  category: "DIY",
+  description: "Working on a DIY art project but feeling stuck. Any artists or creatives able to offer guidance on color schemes and techniques?",
+  user_id: user6.id,
+  solved: false,
+  asker: true
+)
+
+Post.create!(
+  title: "DIY Plumbing Woes",
+  category: "DIY",
+  description: "Leaky faucet crisis! Anyone with plumbing skills able to lend a hand? A quick fix could save me a call to the plumber. Tools and expertise appreciated!",
+  user_id: user7.id,
+  solved: false,
+  asker: true
+)
+
+Post.create!(
+  title: "Moving Assistance",
   category: "household",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-  user_id: user3.id,
+  description: "Help needed moving furniture to a new place. Will provide snacks, drinks, and good company! Just a few strong arms required.",
+  user_id: user8.id,
+  solved: false,
+  asker: true
+)
+
+Post.create!(
+  title: "Fitness Buddy Wanted",
+  category: "fitness",
+  description: "Looking for a workout partner for evening walks around the neighborhood. Let's motivate each other to stay fit and healthy!",
+  user_id: user9.id,
+  solved: false,
+  asker: true
+)
+
+Post.create!(
+  title: "Home Organization Tips",
+  category: "cleaning",
+  description: "Overwhelmed with clutter and seeking advice on effective organization. Share your best organization hacks, please! From closets to kitchen spaces.",
+  user_id: user10.id,
+  solved: false,
+  asker: true
+)
+
+Post.create!(
+  title: "Tech Recycling",
+  category: "other",
+  description: "Have old electronics to recycle. Anyone aware of nearby e-waste disposal options? Let's ensure our old gadgets are disposed of responsibly.",
+  user_id: user11.id,
+  solved: false,
+  asker: true
+)
+
+Post.create!(
+  title: "Painting Assistance",
+  category: "DIY",
+  description: "Painting a room but struggling with the edges. Any painting experts available to share tips or even lend a steady hand for a few hours?",
+  user_id: user12.id,
   solved: false,
   asker: true
 )
