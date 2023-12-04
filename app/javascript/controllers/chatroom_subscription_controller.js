e583b13d79d4992f10a95097383383284b8ac0dd
Anna Bothe <annabothe0@gmail.com>
2023-12-04 15:09:46 +0100

    added insert message at end, scroll down, and reset form for sender

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -8,8 +8,15 @@ export default class extends Controller {
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => console.log(data) }
+      { received: data => this.#insertMessageAndScrollDown(data) }
     )
     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+  resetForm(e) {
+    e.target.reset()
   }
 }
