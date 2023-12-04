bbdc22b331266860b373449da20140206c602db0
Anna Bothe <annabothe0@gmail.com>
2023-12-04 14:57:31 +0100

    created message partial for chatrooms show

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -6,6 +6,10 @@ export default class extends Controller {
   static values = { chatroomId: Number }
   static targets = ["messages"]
   connect() {
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: data => console.log(data) }
+    )
     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
   }
 }
