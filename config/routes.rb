9173802b61202d87657b7624bfae6f7befb00b2e
Anna Bothe <annabothe0@gmail.com>
2023-12-04 14:09:43 +0100

    created nested message routes,  message controller, and input field in show

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :posts, only: %i[index show new create]
-  resources :chatrooms, only: :show
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
 end
