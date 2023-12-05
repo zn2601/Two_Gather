b42fba5a8a6f9001f06206d9c205d82367365fdb
Vincent Bohec <vbohec@yahoo.fr>
2023-12-05 13:57:37 +0100

    fixed the posting of reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
       @user = User.find(params[:id]) # params[:id] is the user id from the URL
       @reviews = @user.reviews.order(created_at: :asc) # reviews received by the user as a helper
       @reviews_as_asker = @user.reviews_as_asker.order(created_at: :asc) # reviews received by the user as an asker
+
+
     end
 
 end
