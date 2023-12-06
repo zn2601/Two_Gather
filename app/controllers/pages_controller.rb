4738b3dee3a1db27d8c2ad7c15dc1aaf59d37f00
Zlatko Naumoski <zn2601@gmail.com>
2023-12-06 16:29:24 +0100

    fix dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,13 @@ class PagesController < ApplicationController
 
   def dashboard
     @user = current_user
+    @posts = Post.where(user: current_user)
+
+    posts_reviews = @posts.map {|post| post.reviews}
+    reviews = posts_reviews.flatten
+    @recived_reviews = reviews.select {|review| review.user != current_user}
+
+
   end
 
 end
